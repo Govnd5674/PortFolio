4f835c9f698f39e5910f8058f6530df566587a5d
Govind <23071092@ycce.in>
2025-04-13 15:53:54 +0530

    astronot png done

--- src/components/pages/Page2.jsx
+++ src/components/pages/Page2.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SplitText from "../../blocks/TextAnimations/SplitText/SplitText";
 import ScrollReveal from "../../blocks/TextAnimations/ScrollReveal/ScrollReveal";
 
 const Page2 = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const rocketRef = useRef(null);
+  const astroRef = useRef(null);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      astroRef.current,
+      {
+        x: "100%",
+        y: "-50%",
+        scale: 0.2,
+        opacity: 0,
+        rotate: 45,
+      },
+      {
+        x: "20%",
+        y: "-100%",
+        scale: 2,
+        rotate: 0,
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top center",
+          end: "center center",
+          scrub: true,
+          toggleActions: "play none none reverse"
+        },
+      }
+    );
+  }, []);
+
   return (
     <div className="relative flex h-[100vh] justify-center items-center select-none w-full">
-      <section className="flex flex-col justify-center items-center h-screen max-w-9xl mx-auto px-4 bg-gray-950 text-gray-100">
+      <section ref={sectionRef} className="relative flex flex-col justify-center items-center h-screen max-w-9xl mx-auto px-4 bg-gray-950 text-gray-100">
         <div className="font-Mogra text-center font-bold mb-12">
           <SplitText
             text="ABOUT ME ..."
@@ -15,12 +50,14 @@ const Page2 = () => {
             animationTo={{ opacity: 1, transform: "translateY(0)" }}
           />
         </div>
-        {/* <div className="absolute">
-          <img src="../../assets/png/rocket.png" alt="" />
+        
+        <div className="absolute top-[30%] right-[0%] z-10" ref={astroRef}>
+          <img
+            src="https://github.com/Govnd5674/PortFolio/blob/main/src/assets/png/astro2.png?raw=true"
+            alt="astronaut"
+            className="w-40 h-40 object-contain"
+          />
         </div>
-        <div className="absolute">
-          <img src="../../assets/png/astro2.png" alt="" />
-        </div> */}
         <div className="font-Mogra text-sm font-normal text-gray-500 text-center max-w-6xl">
           <ScrollReveal
             className="text-gray-500 text-sm font-normal"
